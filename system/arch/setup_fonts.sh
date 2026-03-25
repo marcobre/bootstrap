93cb3cfc79e7321ede267835e5205ccc28abbd11
Marco <13064322+marcobre@users.noreply.github.com>
2026-03-25 17:00:19 +0100

    Update setup_fonts.sh

--- system/arch/setup_fonts.sh
+++ system/arch/setup_fonts.sh
@@ -1,16 +1,63 @@
 #!/bin/bash
+
+declare -a fonts=(
+  #  BitstreamVeraSansMono
+  CascadiaCode
+  # CodeNewRoman
+  # DroidSansMono
+  FiraCode
+  FiraMono
+  # Go-Mono
+  # Hack
+  # Hermit
+  JetBrainsMono
+  Meslo
+  # Noto
+  # Overpass
+  # ProggyClean
+  # RobotoMono
+  SourceCodePro
+  # SpaceMono
+  # Ubuntu
+  # UbuntuMono
+)
+
+version=$(curl -s 'https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest' | jq -r '.name')
+fonts_dir="${HOME}/.local/share/fonts"
+
+if [[ ! -d "${fonts_dir}" ]]; then
+  mkdir -p "${fonts_dir}"
+fi
+
+for font in "${fonts[@]}"; do
+  zip_file="${font}.zip"
+  download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/${version}/${zip_file}"
+  echo "Downloading $download_url"
+  wget "$download_url"
+  unzip -o "$zip_file" -d "${fonts_dir}"
+  rm "$zip_file"
+done
+
+find "${fonts_dir}" -name 'Windows Compatible' -delete
+
+fc-cache -fv
+
+
+
+
+#!/bin/bash
 # shellcheck disable=SC1091
 
 #==================================
 # Source utilities
 #==================================
-. "$HOME/.dotfiles/scripts/utils/utils.sh"
-. "$HOME/.dotfiles/scripts/utils/utils_arch.sh"
+#. "$HOME/.dotfiles/scripts/utils/utils.sh"
+#. "$HOME/.dotfiles/scripts/utils/utils_arch.sh"
 
 
 #==================================
 # Print Section Title
 #==================================
-print_section "Installing Fonts"
+#print_section "Installing Fonts"
 
-execute "yay -S otf-firamono-nerd" "Fira Mono Nerd Font"
+#execute "yay -S otf-firamono-nerd" "Fira Mono Nerd Font"
