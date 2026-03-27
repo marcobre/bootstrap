72b6b2943972bdea78074990493252125e14ccfb
Marco Bretzke <bretzke@amazon.com>
2026-03-27 13:08:08 +0100

    changed mangowc package name

--- system/arch/setup_wm.sh
+++ system/arch/setup_wm.sh
@@ -15,23 +15,23 @@ print_section "Installing Window Manager"
 WM="${1:-mangowc}"
 
 case "$WM" in
-    mangowc)
-        print_title "Install MangoWC"
-        yay_install "MangoWC" "mangowc-git"
-        pacman_install "Waybar" "waybar"
-        pacman_install "Swaybg" "swaybg"
-        print_title "MangoWC configuration"
-        symlink ~/.dotfiles/config/mango/config.conf ~/.config/mango/config.conf
-        symlink ~/.dotfiles/config/mango/autostart.sh ~/.config/mango/autostart.sh
-        ;;
-    hyprland)
-        print_title "Install Hyprland"
-        # TODO: Add Hyprland packages
-        print_warning "Hyprland installation not yet implemented"
-        ;;
-    niri)
-        print_title "Install Niri"
-        # TODO: Add Niri packages
-        print_warning "Niri installation not yet implemented"
-        ;;
+mangowc)
+  print_title "Install MangoWC"
+  yay_install "MangoWC" "mangowc"
+  pacman_install "Waybar" "waybar"
+  pacman_install "Swaybg" "swaybg"
+  print_title "MangoWC configuration"
+  symlink ~/.dotfiles/config/mango/config.conf ~/.config/mango/config.conf
+  symlink ~/.dotfiles/config/mango/autostart.sh ~/.config/mango/autostart.sh
+  ;;
+hyprland)
+  print_title "Install Hyprland"
+  # TODO: Add Hyprland packages
+  print_warning "Hyprland installation not yet implemented"
+  ;;
+niri)
+  print_title "Install Niri"
+  # TODO: Add Niri packages
+  print_warning "Niri installation not yet implemented"
+  ;;
 esac
